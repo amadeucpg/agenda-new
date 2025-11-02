92ae3528546cf4fbcedef420cd17bf35a28ab988
Amadeu <amadeucpg@gmail.com>
2025-11-02 12:39:34 -0300

    voltou h1

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@ include_once("templates/header.php");
 
 <div class="container">
   <?php include_once("templates/backbtn.html"); ?>
-  <h1 id="main-title">Editar contatoooo!</h1>
+  <h1 id="main-title">Editar contato!</h1>
   <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
     <input type="hidden" name="type" value="edit">
     <input type="hidden" name="id" value="<?= $contact["id"]; ?>">
